8bb3c61f95aefd0fe4eff303cba5d85788c81c20
youga114 <youga114@gmail.com>
2023-04-06 17:15:23 +0900

    [Cookie] Add log to check cookie

--- middleware.ts
+++ middleware.ts
@@ -7,9 +7,7 @@ export const config = {
 export function middleware(req: NextRequest) {
     console.log(req.cookies);
     const access_token = req.cookies.get("access_token")?.value || "";
-    if (access_token) return NextResponse.next();
-
-    console.log(access_token);
+    if (!access_token) return NextResponse.next();
 
     const response = NextResponse.next();
     response.cookies.set("access_token", access_token);
