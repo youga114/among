f058508f87ad268b4439a4541cff94b8f7eb5f48
youga114 <youga114@gmail.com>
2023-04-25 11:13:33 +0900

    [Album] Add to sort story

--- store/album.ts
+++ store/album.ts
@@ -10,6 +10,17 @@ const album = createSlice({
     initialState,
     reducers: {
         setPages(state, action: PayloadAction<pageType[]>) {
+            action.payload.sort((left, right) => {
+                if (left.date > right.date) {
+                    return -1;
+                }
+                if (left.date < right.date) {
+                    return 1;
+                }
+
+                return 0;
+            });
+
             state.pages = action.payload;
         }
     }
